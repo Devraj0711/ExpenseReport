b13352c90ba005674263cd4598dbf3476f8c568a
Devraj0711 <69614639+Devraj0711@users.noreply.github.com>
2023-06-19 08:10:18 -0700

    Update Expense.js

--- Expense.js
+++ Expense.js
@@ -1,72 +1,59 @@
-const myExpense=document.querySelector('#my-Expense');
-const item_Id=document.getElementById("listOfExpenses"); 
+const myExpense = document.querySelector('#my-Expense');
+const item_Id = document.getElementById("listOfExpenses");
 
 //to add event listeners
-myExpense.addEventListener('submit',onSubmit);
+myExpense.addEventListener('submit', onSubmit);
 item_Id.addEventListener('click', removeItem);
 
-//assiging variables to the values
-const Exp_Amt=document.getElementById('ExpAmt');
-const Exp_des=document.getElementById('Dsp');
-const Exp_categ=document.getElementById('Category');  
+//assigning variables to the values
+const Exp_Amt = document.getElementById('ExpAmt');
+const Exp_des = document.getElementById('Dsp');
+const Exp_categ = document.getElementById('Category');
 
-    function onSubmit(e)
-      {
-        
-        e.preventDefault();
-        console.log(Exp_Amt.value);
-        console.log(Exp_des.value);
-        console.log(Exp_categ.value);
-        let myobj={my_amt: Exp_Amt.value, my_des:Exp_des.value, my_categ:Exp_categ.value};
-        let MyObj=JSON.stringify(myobj);
-        localStorage.setItem(Exp_des.value, MyObj);
-        showOnScreen(myobj);
-      }
-      function showOnScreen(myobj)
-      {
-        //e.preventDefault();
-        console.log(myobj.my_amt);
-        const parentElement=document.getElementById("listOfExpenses");
-        const childElement=document.createElement('li');
-        //create a delete button
-        var DeleteBtn= document.createElement('button');
-        DeleteBtn.className='btn btn-danger btn-sm float-right delete';
-        DeleteBtn.appendChild(document.createTextNode(' Delete Expense '));
+function onSubmit(e) {
+  e.preventDefault();
+  console.log(Exp_Amt.value);
+  console.log(Exp_des.value);
+  console.log(Exp_categ.value);
+  let myobj = { my_amt: Exp_Amt.value, my_des: Exp_des.value, my_categ: Exp_categ.value };
+  let MyObj = JSON.stringify(myobj);
+  localStorage.setItem(Exp_des.value, MyObj);
+  showOnScreen(myobj);
+}
 
-        childElement.textContent=myobj.my_amt+' - '+myobj.my_des+' - '+myobj.my_categ;
-        childElement.appendChild(DeleteBtn);
-        parentElement.appendChild(childElement);
+function showOnScreen(myobj) {
+  const parentElement = document.getElementById("listOfExpenses");
+  const childElement = document.createElement('li');
+  childElement.textContent = myobj.my_amt + ' - ' + myobj.my_des + ' - ' + myobj.my_categ;
 
-        //edit
-        //edit button
-        const editButton=document.createElement('input');
-        editButton.type="button";
-        editButton.value="Edit";
-        editButton.onclick=()=>{
-        localStorage.removeItem(Exp_des.value);
-        parentElement.removeChild(childElement);
-        document.getElementById('ExpAmt').value=Exp_Amt.value;
-        document.getElementById('Dsp').value=Exp_des.value;
-        document.getElementById('Category').value=Exp_categ.value;
-      }
-    childElement.appendChild(DeleteBtn);
-    childElement.appendChild(editButton);
-    parentElement.appendChild(childElement);
+  const deleteButton = document.createElement('button');
+  deleteButton.className = 'btn btn-danger btn-sm float-right delete';
+  deleteButton.appendChild(document.createTextNode('Delete Expense'));
+  
+  const editButton = document.createElement('button');
+  editButton.className = 'btn btn-primary btn-sm float-right edit';
+  editButton.appendChild(document.createTextNode('Edit'));
+  
+  childElement.appendChild(editButton);
+  childElement.appendChild(deleteButton);
 
-        
-      }
+  parentElement.appendChild(childElement);
 
-      function removeItem(e)
-     {
-    e.preventDefault();
-      if(e.target.classList.contains('delete'))
-       {
-          if(confirm('Sure man?'))
-          {
-            var li=e.target.parentElement;
-            item_Id.removeChild(li);
-            
-            localStorage.removeItem(Exp_des.value);
-          }
-        }
-       }
+  editButton.addEventListener('click', function () {
+    localStorage.removeItem(myobj.my_des);
+    parentElement.removeChild(childElement);
+    document.getElementById('ExpAmt').value = myobj.my_amt;
+    document.getElementById('Dsp').value = myobj.my_des;
+    document.getElementById('Category').value = myobj.my_categ;
+  });
+}
+
+function removeItem(e) {
+  if (e.target.classList.contains('delete')) {
+    if (confirm('Sure man?')) {
+      var li = e.target.parentElement;
+      item_Id.removeChild(li);
+      localStorage.removeItem(Exp_des.value);
+    }
+  }
+}
